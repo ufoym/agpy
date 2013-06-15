f8c674dea6a69836987b9bc0c5b10556348ff0f1
keflavich <keflavich@7a03b35a-38c0-11de-8fef-1f5ae417a2df>
2013-06-15 16:33:08 +0000

    pymc plotting: pep8 fixes, corrected issue with colorbar and oo vs state treatment

--- trunk/agpy/pymc_plotting.py
+++ trunk/agpy/pymc_plotting.py
@@ -17,28 +17,31 @@ def find_percentile(data, pctile):
 def errellipse(MC, varname1, varname2, ax=None):
     N = pymc.NormApprox(MC)
     N.fit()
-    E =  matplotlib.patches.Ellipse(N.mu[N.__dict__[varname1],
-        N.__dict__[varname2]], N.C[N.__dict__[varname1]],
-        N.C[N.__dict__[varname2]], 
-        (N.C[N.__dict__[varname1], N.__dict__[varname2]][0,1] /
-            N.C[N.__dict__[varname1]] * 90.)[0],
-        facecolor='none', edgecolor='black')
+    E = matplotlib.patches.Ellipse(N.mu[N.__dict__[varname1],
+                                   N.__dict__[varname2]],
+                                   N.C[N.__dict__[varname1]],
+                                   N.C[N.__dict__[varname2]],
+                                   (N.C[N.__dict__[varname1],
+                                    N.__dict__[varname2]][0,1] /
+                                    N.C[N.__dict__[varname1]] * 90.)[0],
+                                   facecolor='none',
+                                   edgecolor='black')
     if ax is None:
         ax=pylab.gca()
     ax.add_artist(E)
 
 def hist2d(MC, varname1, varname2, varslice=None,
-        percentiles=[0.0027,0.0455,0.3173,0.5,0.75],
-        colors=[(0.4,0.4,1,0.2),(1,0.4,1,0.5),(1,0.2,0.2,0.5),(0.7,0.1,0.1,1),(0.5,0.05,0.05,1),(0.4,0.05,0.05,0.5)],
-        ticklabels=['3$\\sigma$','2$\\sigma$','1$\\sigma$','50%','25%'],
-        axis=None,
-        fignum=1,
-        contourcmd=pylab.contourf,
-        clear=False,
-        colorbar=True,
-        doerrellipse=False,
-        chain=None,
-        **kwargs):
+           percentiles=[0.0027,0.0455,0.3173,0.5,0.75],
+           colors=[(0.4,0.4,1,0.2),(1,0.4,1,0.5),(1,0.2,0.2,0.5),(0.7,0.1,0.1,1),(0.5,0.05,0.05,1),(0.4,0.05,0.05,0.5)],
+           ticklabels=['3$\\sigma$','2$\\sigma$','1$\\sigma$','50%','25%'],
+           axis=None,
+           fignum=1,
+           contourcmd=pylab.contourf,
+           clear=False,
+           colorbar=True,
+           doerrellipse=False,
+           chain=None,
+           **kwargs):
     """
     Create a 2D histogram of the MCMC data over some Trace range
     """
@@ -59,21 +62,22 @@ def hist2d(MC, varname1, varname2, varslice=None,
     levels = [find_percentile(histvals, p*100) for p in percentiles]
     
     if axis is None:
-        pylab.figure(fignum); 
-        if clear: pylab.clf(); 
+        pylab.figure(fignum)
+        if clear:
+            pylab.clf()
         axis = pylab.gca()
 
     xax = np.linspace(xvals.min(),xvals.max(),histvals.shape[1])
     yax = np.linspace(yvals.min(),yvals.max(),histvals.shape[0])
     if axis is not None:
         contourcmd = eval('axis.'+contourcmd.__name__)
-    contourcmd(xax, yax, histvals.swapaxes(0,1), levels+[histvals.max()], colors=colors)
-    axis.set_xlabel(varname1); 
-    axis.set_ylabel(varname2); 
-    if colorbar: 
+    cntr = contourcmd(xax, yax, histvals.swapaxes(0,1), levels+[histvals.max()], colors=colors)
+    axis.set_xlabel(varname1)
+    axis.set_ylabel(varname2)
+    if colorbar:
         try:
-            cb = pylab.colorbar(); 
-            cb.ax.set_yticks(levels); 
+            cb = pylab.colorbar(cntr, ax=axis)
+            cb.ax.set_yticks(levels)
             cb.ax.set_yticklabels(ticklabels)
         except Exception as e:
             print "Colorbar failed with exception ",e
@@ -83,18 +87,18 @@ def hist2d(MC, varname1, varname2, varslice=None,
 
 
 def gkde_contours(MC, varname1, varname2, varslice=None,
-        percentiles=[0.0027,0.0455,0.3173,0.5,0.75],
-        colors=[(0.4,0.4,1,0.2),(1,0.4,1,0.5),(1,0.2,0.2,0.75),(1,0.1,0.1,1),(0.8,0.0,0.0,1),(0,0,0,1)],
-        ticklabels=['3$\\sigma$','2$\\sigma$','1$\\sigma$','50%','75%'],
-        fignum=1,
-        ngridpts=101,
-        clear=False,):
+                  percentiles=[0.0027,0.0455,0.3173,0.5,0.75],
+                  colors=[(0.4,0.4,1,0.2),(1,0.4,1,0.5),(1,0.2,0.2,0.75),(1,0.1,0.1,1),(0.8,0.0,0.0,1),(0,0,0,1)],
+                  ticklabels=['3$\\sigma$','2$\\sigma$','1$\\sigma$','50%','75%'],
+                  fignum=1,
+                  ngridpts=101,
+                  clear=False,):
     """
     Contours for kernel densit estimate... to compare to real contours
     """
     import scipy.stats
     data1 = MC.trace(varname1)[slice(*varslice)]
-    data2 = MC.trace(varname2)[slice(*varslice)] 
+    data2 = MC.trace(varname2)[slice(*varslice)]
     gkde = scipy.stats.gaussian_kde([data1,data2])
     xvals = np.linspace(data1.min(),data1.max(),ngridpts)
     yvals = np.linspace(data2.min(),data2.max(),ngridpts)
@@ -102,16 +106,18 @@ def gkde_contours(MC, varname1, varname2, varslice=None,
 
     zz = np.array(gkde.evaluate([xx.flatten(),yy.flatten()])).reshape(xx.shape)
 
-    pylab.figure(fignum); 
-    if clear: pylab.clf(); 
+    pylab.figure(fignum)
+    if clear:
+        pylab.clf()
 
-    contour(xx, yy, zz, linewidths=1, alpha=.5, cmap=cm.Greys)
+    pylab.contour(xx, yy, zz, linewidths=1, alpha=.5, cmap=matplotlib.cm.Greys)
     
-    pylab.xlabel(varname1); 
-    pylab.ylabel(varname2); 
+    pylab.xlabel(varname1)
+    pylab.ylabel(varname2)
 
 
-def plot_mc_hist(MC,field,varslice=None,onesided=True,bins=50,chain=None,**kwargs):
+def plot_mc_hist(MC, field, varslice=None, onesided=True, bins=50, chain=None,
+                 **kwargs):
     """
     Plot a histogram with 1,2,3-sigma bars
     """
@@ -131,23 +137,23 @@ def plot_mc_hist(MC,field,varslice=None,onesided=True,bins=50,chain=None,**kwarg
         field_stats['quantiles'] = {q:np.percentile(field_data,q) for q in [0.135,2.275,15.866,84.134,97.725,99.865,50]}
 
     vpts = field_stats['quantiles']
-    field_data_sorted = np.sort(field_data)
+    #field_data_sorted = np.sort(field_data)
     h,l,p = pylab.hist(field_data,bins=bins,histtype='stepfilled',**kwargs)
     ax = pylab.gca()
     if kwargs.get('normed'):
         ylim = [0,h.max()*1.01]
     else:
         ylim = ax.get_ylim()
-    fieldlen = len(field_data)
+    #fieldlen = len(field_data)
     if onesided:
-        pylab.vlines(vpts[68.2689] ,*ylim ,linewidth=3, alpha=0.5, color='k',label="$1\\sigma$")
-        pylab.vlines(vpts[95.44997],*ylim ,linewidth=3, alpha=0.5, color='r',label="$2\\sigma$")
-        pylab.vlines(vpts[99.7300] ,*ylim ,linewidth=3, alpha=0.5, color='g',label="$3\\sigma$")
+        pylab.vlines(vpts[68.2689], *ylim,linewidth=3, alpha=0.5, color='k',label="$1\\sigma$")
+        pylab.vlines(vpts[95.44997],*ylim,linewidth=3, alpha=0.5, color='r',label="$2\\sigma$")
+        pylab.vlines(vpts[99.7300], *ylim,linewidth=3, alpha=0.5, color='g',label="$3\\sigma$")
     else:
         pylab.vlines(field_stats['mean'],*ylim,color='k', linestyle='--', linewidth=3, alpha=0.5, label="$\mu$")
         pylab.vlines(vpts[50],*ylim, color='b', linestyle='--', linewidth=3, alpha=0.5, label="$\mu_{1/2}$")
         pylab.vlines([vpts[15.866],vpts[84.134]],*ylim,color='k',linewidth=3, alpha=0.5, label="$1\\sigma$")
-        pylab.vlines([vpts[ 2.275],vpts[97.725]],*ylim,color='r',linewidth=3, alpha=0.5, label="$2\\sigma$")
-        pylab.vlines([vpts[ 0.135],vpts[99.865]],*ylim,color='g',linewidth=3, alpha=0.5, label="$3\\sigma$")
+        pylab.vlines([vpts[02.275],vpts[97.725]],*ylim,color='r',linewidth=3, alpha=0.5, label="$2\\sigma$")
+        pylab.vlines([vpts[00.135],vpts[99.865]],*ylim,color='g',linewidth=3, alpha=0.5, label="$3\\sigma$")
     ax.set_ylim(*ylim)
     pylab.legend(loc='best')
